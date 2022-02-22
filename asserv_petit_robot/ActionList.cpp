30f38875e39f473560029a33e26dc2df05b82880
Thomas Girod <thgirod@hotmail.com>
2022-02-22 16:03:55 +0100

    added default constructor to ActionList

--- asserv_petit_robot/ActionList.cpp
+++ asserv_petit_robot/ActionList.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "ActionList.h"
+
+
+ActionList::ActionList() : m_size(0), m_actions{nullptr} {}
+
+void ActionList::add(Action *new_action) {
+    m_actions[m_size++] = new_action;
+}
+
+void ActionList::add(Action *new_action, size_t index) {
+    if (index < m_size)
+        memmove(m_actions + index, m_actions + index + 1, m_size - index);
+    m_actions[index] = new_action;
+    m_size++;
+}
+
+ActionList &ActionList::operator+=(Action *new_action) {
+    this->add(new_action);
+    return *this;
+}
+
+Action* ActionList::remove() {
+    return m_actions[m_size--];
+}
+
+Action *ActionList::remove(size_t index) {
+    if (index < m_size)
+        return nullptr;
+    Action* result = m_actions[index];
+    m_size--;
+    memmove(m_actions + index + 1, m_actions + index, m_size - index);
+    return result;
+}
+
+bool ActionList::is_empty() const {
+    return m_size == 0;
+}
+
+bool ActionList::is_full() const {
+    return m_size == MAX_ACTION_NBR;
+}
+
+Action *ActionList::operator[](size_t index) {
+    if (m_size < index) return nullptr;
+    return m_actions[index];
+}
--- asserv_petit_robot/ActionList.h
+++ asserv_petit_robot/ActionList.h
@@ -5,12 +5,27 @@
 #ifndef ASSERV_PETIT_ROBOT_ACTIONLIST_H
 #define ASSERV_PETIT_ROBOT_ACTIONLIST_H
 
+#include <Arduino.h>
+#include "Action.h"
 
+#define MAX_ACTION_NBR 15
 
 class ActionList {
+    public:
+        ActionList();
+        void add(Action* new_action);
+        void add(Action* new_action, size_t index);
+        ActionList &operator+=(Action *new_action);
+        Action* remove();
+        Action* remove(size_t index);
+        inline bool is_empty() const;
+        inline bool is_full() const;
+        Action* operator[](size_t index);
 
+    private:
+        Action *m_actions[MAX_ACTION_NBR];
+        size_t m_size;
 };
-
 
 
 #endif //ASSERV_PETIT_ROBOT_ACTIONLIST_H
