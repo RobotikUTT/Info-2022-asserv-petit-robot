c44993867e779fd26d88f585f2138ed6cf6b8cf1
Gregory Holder <gregory.holder76@gmail.com>
2022-02-23 13:09:44 +0100

    robot structure

--- asserv_petit_robot/Robot.cpp
+++ asserv_petit_robot/Robot.cpp
@@ -1,36 +1,44 @@
-// #include "Robot.h"
+#include "Robot.h"
+#include "Wheel.h"
 
-// namespace Robot {
-//     namespace {
-//         Encoder right_encoder(encoderRightA, encoderRightB);
-//         Encoder left_encoder(encoderLeftA, encoderLeftB);
-//         AsservedWheel right_wheel(RightWheelEnable, RightWheelControl1, RightWheelControl2, encoderRightA, encoderRightB);
-//         AsservedWheel left_wheel(LeftWheelEnable, LeftWheelControl1, LeftWheelControl2, encoderLeftA, encoderLeftB);
-//     }
+namespace Robot {
+    namespace {
+        Wheel left_wheel(LEFT_WHEEL_ENABLE, LEFT_WHEEL_CONTROL1, LEFT_WHEEL_CONTROL2, ENCODER_LEFT_A, ENCODER_LEFT_B, MOTOR_SPEED_P, MOTOR_SPEED_I, MOTOR_SPEED_D, MOTOR_POS_P, MOTOR_POS_I, MOTOR_POS_D);
+        Wheel right_wheel(RIGHT_WHEEL_ENABLE, RIGHT_WHEEL_CONTROL1, RIGHT_WHEEL_CONTROL2, ENCODER_RIGHT_A, ENCODER_RIGHT_B, MOTOR_SPEED_P, MOTOR_SPEED_I, MOTOR_SPEED_D, MOTOR_POS_P, MOTOR_POS_I, MOTOR_POS_D);
+        position_t robot_position;
+        double robot_angle;
+    }
 
-//     void move(const int8_t p_speed) {
-//         const uint8_t cmd_speed = abs(p_speed);
-//         if (p_speed > 0) {
-//             right_wheel.move_forward(cmd_speed);
-//             left_wheel.move_forward(cmd_speed);
-//         } else if (p_speed < 0) {
-//             right_wheel.move_backward(cmd_speed);
-//             left_wheel.move_backward(cmd_speed);
-//         } else {
-//             right_wheel.stop();
-//             left_wheel.stop();
-//         }
-//     }
+    void update();
 
-//     void stop() {
-//         right_wheel.stop();
-//         left_wheel.stop();
-//     }
+    void set_speed(double speed);
+    
+    void stop();
+    void resume();
 
-//     void compute_pid() {
-//         right_wheel.compute_pid();
-//         left_wheel.compute_pid();
-//     }
-// }
+    namespace rotate{
+        void absolute(double angle);
+        void relative(double angle);
+        void look_at(position_t position);
+    }
+
+    namespace move{
+        void distance(int16_t distance);
+        void to_position(position_t position);
+    }
+
+    namespace set{
+        void position(position_t position);
+        void angle(double angle);
+    }
+
+    namespace get{
+        position_t position();
+        double angle();
+    }
+
+    
+    
+}
 
 
--- asserv_petit_robot/Robot.h
+++ asserv_petit_robot/Robot.h
@@ -1,36 +1,54 @@
-// #ifndef ROBOT_H
-// #define ROBOT_H
+#ifndef ROBOT_H
+#define ROBOT_H
 
-// #include <Arduino.h>
-// #include <Encoder.h>
-// #include <PID_v1.h>
+#include <Encoder.h>
+#include <PID_v1.h>
 
-// #include "Wheel.h"
-// #include "pins.h"
-// #include "AsservedWheel.h"
+#include "Wheel.h"
+#include "pins.h"
 
-
-// #define TICKS_PER_REVOLUTION 2248.86
-// #define WHEEL_DIAMETER 0.074
-// #define WHEEL_PERIMETER 0.2325
-// #define REVOLUTIONS_PER_METER 4.3015  // 1/WHEEL_PERIMETER = 1/0.2325
-// #define TICKS_PER_METER 9673.44  // 2248.86 * 4.3015
-
-// #define SAMPLING_FREQUENCY 50.0
-// #define NEGLIGIBLE_ERROR 0.01
+#define TICKS_PER_REVOLUTION 2248.86
+#define WHEEL_DIAMETER 0.074
+#define WHEEL_PERIMETER 0.2325
+#define REVOLUTIONS_PER_METER 4.3015  // 1/WHEEL_PERIMETER = 1/0.2325
+#define TICKS_PER_METER 9673.44  // 2248.86 * 4.3015
 
 
-// typedef struct {
-//     double x;
-//     double y;
-//     double angle;
-// } position_t;
+typedef struct {
+    double x;
+    double y;
+} position_t;
 
 
-// namespace Robot {
-//     void move(const int8_t p_speed);
-//     void stop();
-//     void compute_pid();
-// }
+namespace Robot {
 
-// #endif
+    namespace rotate{
+        void absolute(double angle);
+        void relative(double angle);
+        void look_at(position_t position);
+    }
+
+    namespace move{
+        void distance(int16_t distance);
+        void to_position(position_t position);
+    }
+
+    namespace set{
+        void position(position_t position);
+        void angle(double angle);
+    }
+
+    namespace get{
+        position_t position();
+        double angle();
+    }
+
+    void set_speed(double speed);
+    
+    void stop();   // Interompre l'action
+    void resume(); // Continuer l'action
+    
+    void update(); // Appelé à chaque boucle
+}
+
+#endif
